c6d23c1768068f6e9a9b8d262d0d3d7dac2106c9
久我友大 <tomohirokuga@koganotomodainoMacBook-Pro.local>
2020-11-10 17:11:08 +0900

    update log_watch

--- log_watch.py
+++ log_watch.py
@@ -7,8 +7,22 @@ from watchdog.ovservers import Observer
 import time
 import os
 
+# 監視対象のディレクトリを指定
+target_dir = 'C:\Users\forechem2\Desktop\ODVeiwer'
+# 監視対象ファイルのパターンマッチを指定
+target_file = '*.txt'
+
 class ChangeHandler(PatternMatchingEventHandler):
+    def __init__(self, command, patterns):
+        super(ChangeHandler, self).__init__(patterns=patterns)
+        self.command = command
+
+    def _run_command(self):
+        subprocess.call([self.command, ])
+
     def on_modified(self, event):
+        print("data coming")
+        #self._run_command()
         """
         ここをload_data.pyを実行するように変更する
         """
@@ -16,7 +30,7 @@ class ChangeHandler(PatternMatchingEventHandler):
 observer = Observer()
 
 #監視するフォルダを第2引数に指定
-observer.schedule(ChangeHandler(), './work', recursive=True)
+observer.schedule(ChangeHandler([target_file]), target_dir, recursive=True) #どこでtarget_dir指定する？
 
 #監視を開始
 observer.start()
