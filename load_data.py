b4cded6aec3bbf7c04655e25ef5a624ee193ea20
久我友大 <tomohirokuga@koganotomodainoMacBook-Pro.local>
2020-11-13 14:29:28 +0900

    successfully loading data with modified log_watch.py and load_data.py

--- load_data.py
+++ load_data.py
@@ -1,1 +1,58 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import csv
+import sys
+# influxdb_client　APIを利用
+from influxdb_client import InfluxDBClient, Point, WritePrecision, Dialect
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+# You can generate a Token from the "Tokens Tab" in the UI
+token = "xxx"
+org = "xxx"
+bucket = "xxx"
+
+client = InfluxDBClient(url="xxx", token=token)
+
+
+def load_data(filename):
+    with open(filename) as f:
+        reader = csv.reader(f)
+        l = [row for row in reader]
+        
+    df = pd.DataFrame(l[12:])
+    df.columns = ['Timestamp', 'time','OD_01', 'OD_02', 'OD_03', 'OD_04', 'OD_05', 'OD_06', 'OD_07', 'OD_08', 'temp']
+    # df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+    df_2 = df.tail(1)
+    col = list(df_2)
+    
+    # value dtypes to float
+    for i in col[2:10]:
+        df_2[i] = pd.to_numeric(df_2[i])
+    
+    # dtype of timestamp to datetime
+    df_2['Timestamp'] = pd.to_datetime(df_2['Timestamp'])
+    print(df_2)
+    
+    ## Write Pandas DataFrame
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    query_api = client.query_api()
+    
+    data_frame = pd.DataFrame(data=[["OD_01", df_2.iat[0, 2]], ["OD_02", df_2.iat[0, 3]], ["OD_03", df_2.iat[0, 4]], ["OD_04", df_2.iat[0, 5]], ["OD_05", df_2.iat[0, 6]], ["OD_06", df_2.iat[0, 7]], ["OD_07", df_2.iat[0, 8]], ["OD_08", df_2.iat[0, 9]]],
+                           index=[datetime.utcnow(), datetime.utcnow(), datetime.utcnow(), datetime.utcnow(), datetime.utcnow(), datetime.utcnow(), datetime.utcnow(), datetime.utcnow()],
+                           columns=["sensor", "PV"])
+    print(df_2.iat[0, 0])
+    print(datetime.utcnow())
+    write_api.write(bucket, org, record=data_frame, data_frame_measurement_name='mea1',
+                    data_frame_tag_columns=['sensor'])
+                    
+    ## Close client
+    write_api.__del__()
+    client.__del__()
+
+# execute with target file name
+if __name__ == "__main__":
+    load_data(sys.argv[1])
+
+
 
--- log_watch.py
+++ log_watch.py
@@ -1,6 +1,6 @@
 # watchdogでファイルを監視する
 # ファイルの最終行が変更されたら、load_data.pyを実行する
-# 1分に1度ファイルの監視を走らせる... shakerのデータ取得最小単位が1 minなので
+# 1秒に1度ファイルの監視を走らせる... shakerのデータ取得最小単位が1 minなので
 
 
 from watchdog.events import PatternMatchingEventHandler
@@ -8,33 +8,27 @@ from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 import time
 import os
+import sys
+import subprocess
 
 target_dir = 'C:\\Users\\xxx\\Desktop\\ODVeiwer'
-target_file = '*.txt'
 
 class ChangeHandler(FileSystemEventHandler):
-   # def __init__(self, command, patterns):
-       # super(ChangeHandler, self).__init__(patterns=patterns)
-       # self.command = command
-
-  #  def _run_command(self):
-       # subprocess.call([self.command, ])
-
     def on_modified(self, event):
-        filepath = event.src_path
-        filename = os.path.basename(filepath)
-        print("data coming")
-        
+        filepath = event.src_path # modified file path here
+        filename = os.path.basename(filepath) # modified file name here 
+        print("%s data coming" % filename)
+        subprocess.call('python load_data.py %s' % filename, shell=True)
 
+# execute every one second       
 if __name__ == "__main__":
-     event_handler = ChangeHandler()
-     observer = Observer()
-     observer.schedule(event_handler, target_dir, recursive=True)
-     observer.start()
-     try:
-         while True:
-             time.sleep(1)
-     except KeyboardInterrupt:
-         observer.stop()
-     observer.join()
-
+    event_handler = ChangeHandler()
+    observer = Observer()
+    observer.schedule(event_handler, target_dir, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
