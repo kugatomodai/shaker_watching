7c0b59a0f701a164516f1f823e81fdb3f71a3962
kugatomodai <41180719+kugatomodai@users.noreply.github.com>
2020-11-24 11:05:11 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -7,6 +7,7 @@ import sys
 from influxdb_client import InfluxDBClient, Point, WritePrecision, Dialect
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+
 # You can generate a Token from the "Tokens Tab" in the UI
 token = "xxx"
 org = "xxx"
